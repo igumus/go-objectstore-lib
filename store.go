3b1beabb95142a2da7d4ee82f8803fdec98956c7
igumus <ibrhm.gumus@gmail.com>
2022-06-11 20:18:55 +0300

    refactored: changed to event channel in ListObject function return type

--- store.go
+++ store.go
@@ -5,11 +5,17 @@ import (
 	"io"
 )
 
+// ListObjectEvent - handles async listing objects of objectstore
+type ListObjectEvent struct {
+	Object string
+	Error  error
+}
+
 // ObjectStore defines the functions clients need to perform
 // read/write/existence/linking operations on objectstore.
 type ObjectStore interface {
 	CreateObject(context.Context, io.Reader) (string, error)
 	ReadObject(context.Context, string) ([]byte, error)
 	HasObject(context.Context, string) bool
-	ListObject(context.Context) (<-chan string, <-chan error)
+	ListObject(context.Context) <-chan ListObjectEvent
 }
