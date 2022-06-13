55238e65b01a24c675d93f6fa39612615d8d34a3
igumus <ibrhm.gumus@gmail.com>
2022-06-13 20:10:11 +0300

    updated: objectstore functions to accept/provide cid

--- store.go
+++ store.go
@@ -3,19 +3,21 @@ package objectstore
 import (
 	"context"
 	"io"
+
+	"github.com/ipfs/go-cid"
 )
 
 // ListObjectEvent - handles async listing objects of objectstore
 type ListObjectEvent struct {
-	Object string
+	Object cid.Cid
 	Error  error
 }
 
 // ObjectStore defines the functions clients need to perform
 // read/write/existence/linking operations on objectstore.
 type ObjectStore interface {
-	CreateObject(context.Context, io.Reader) (string, error)
-	ReadObject(context.Context, string) ([]byte, error)
-	HasObject(context.Context, string) bool
+	CreateObject(context.Context, io.Reader) (cid.Cid, error)
+	ReadObject(context.Context, cid.Cid) ([]byte, error)
+	HasObject(context.Context, cid.Cid) bool
 	ListObject(context.Context) <-chan ListObjectEvent
 }
